f67fc143771aa3140f708a9f8a0c9ac19fdb188d
Vinicius Mello <v.mello@hotmail.com>
2018-01-04 22:46:46 -0200

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -58,6 +58,23 @@ class Category extends Model{
                     ]);
         }
     }
+    public function getProductsPage($page = 1, $itensPerPage = 8){
+        $start = ($page - 1) * $itensPerPage;
+        $sql = new Sql();
+        $result = $sql->select("SELECT SQL_CALC_FOUND_ROWS * FROM tb_products a
+            INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+            INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+            WHERE c.idcategory = :idcategory
+            LIMIT $start, $itensPerPage", array(
+                ":idcategory"=> $this->getidcategory()
+            ));
+        $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal");
+        return array(
+            "data"=>Product::checkList($result),
+            "total"=>(int)$resultTotal[0]["nrtotal"],
+            "pages"=> ceil($resultTotal[0]["nrtotal"] / $itensPerPage)
+        );
+    }
     public function addProduct(Product $product){
         $sql = new Sql();
         $sql->query("INSERT INTO tb_productscategories (idcategory, idproduct) VALUES (:idcategory, :idproduct)", array(
